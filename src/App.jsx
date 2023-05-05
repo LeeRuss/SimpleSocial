f9dbaddf69ddb14a4c05511d760629c4baa794ad
LeeRuss <96124920+LeeRuss@users.noreply.github.com>
2023-05-05 15:33:20 +0200

    restructured catalogs and App container

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,25 @@ import SignInFooter from './app/signIn/SignInFooter';
 import SignInHeader from './app/signIn/SignInHeader';
 import Header from './app/signIn/Header';
 import Footer from './app/signIn/Footer';
+import { Link, Outlet } from 'react-router-dom';
 import awsconfig from './aws-exports';
+import styled from 'styled-components';
 
 Amplify.configure(awsconfig);
-
-export const UserContext = createContext();
 
 function App({ signOut, user }) {
   console.log(user);
   return (
-    <UserContext.Provider value={{ user, signOut }}>
-      <div className="App">
-        <h1>Hello {user.attributes.nickname}</h1>
-        <button onClick={signOut}>Sign Out</button>
-      </div>
-    </UserContext.Provider>
+    <div className="App">
+      <nav>
+        <button></button>
+        <Link to="/"> Home</Link>
+      </nav>
+
+      <Outlet context={user} />
+      <h1>Hello {user.attributes.nickname}</h1>
+      <button onClick={signOut}>Sign Out</button>
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppContainer from './AppContainer';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Profile from './app/profile/Profile';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AppContainer />
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Profile />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
