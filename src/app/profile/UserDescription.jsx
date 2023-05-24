7ed1e3469224ca9cb58335fb01236741c65138fe
LeeRuss <96124920+LeeRuss@users.noreply.github.com>
2023-05-24 20:43:52 +0200

    Added option of editing user descirption
    For now only works visual side as the backend part is broken

--- src/app/profile/UserDescription.jsx
+++ src/app/profile/UserDescription.jsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 import { API, graphqlOperation } from 'aws-amplify';
 import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { UserContext } from '../../App';
+import { useContext } from 'react';
+import { updateUsers } from '../../graphql/mutations';
 
 const UserDescriptionContainer = styled.div`
   grid-row-start: profile-start;
@@ -13,6 +17,14 @@ const UserDescriptionContainer = styled.div`
   color: hsl(210, 25%, 25%);
 `;
 
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 const NicknameSpan = styled.span`
   margin-top: 1vh;
   font-size: 2em;
@@ -21,6 +33,46 @@ const NicknameSpan = styled.span`
 
 const UserDescriptionSpan = styled.span`
   font-size: 1.2em;
+  justify-self: center;
+  width: 100%;
+`;
+
+const EditButton = styled.button`
+  align-self: flex-end;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 1%;
+  width: 20px;
+  font-size: 20px;
+  z-index: 2;
+  color: hsl(220, 50%, 50%);
+  background-color: rgba(255, 255, 255, 0);
+`;
+
+const TextArea = styled.textarea`
+  width: 98% !important;
+  height: 100%;
+  font-size: 1.1em;
+  text-align: justify;
+  align-self: center;
+  margin-bottom: 1%;
+  padding: 12px 20px;
+  box-sizing: border-box;
+  border: 3px solid hsl(220, 50%, 50%);
+  border-radius: 4px;
+  background-color: rgba(255, 255, 255, 0.7);
+  color: black;
+  resize: none;
+`;
+
+const FormButtonsContainer = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+`;
+
+const ErrorSpan = styled.span`
+  color: #c50404;
 `;
 
 function getUser(user) {
@@ -34,8 +86,47 @@ function getUser(user) {
 }
 
 export default function UserDescription({ user }) {
+  const userContext = useContext(UserContext);
   const [userData, setUserData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [isEditing, setIsEditing] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    clearErrors,
+    setError,
+    reset,
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      await API.graphql({
+        query: updateUsers,
+        variables: {
+          input: {
+            id: userData.id,
+            description: data.description,
+          },
+        },
+      });
+      clearErrors();
+      changeEditing();
+    } catch (error) {
+      console.log(error);
+      setError('server', {
+        type: 'serverError',
+        message: 'We are not able to update description. Try again later.',
+      });
+    }
+  };
+
+  const changeEditing = () => {
+    if (isEditing) {
+      setIsEditing(false);
+    } else setIsEditing(true);
+    reset();
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -51,6 +142,7 @@ export default function UserDescription({ user }) {
     };
     fetchData();
   }, [user]);
+
   if (isLoading) {
     return <UserDescriptionContainer></UserDescriptionContainer>;
   }
@@ -58,7 +150,58 @@ export default function UserDescription({ user }) {
   return (
     <UserDescriptionContainer>
       <NicknameSpan>{userData.nickname}</NicknameSpan>
-      <UserDescriptionSpan>{userData.description}</UserDescriptionSpan>
+      <Container>
+        {isEditing ? (
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            style={{ width: '100%', display: 'flex', flexDirection: 'column' }}
+          >
+            <TextArea
+              {...register('description', {
+                required: true,
+                maxLength: 200,
+              })}
+              defaultValue={userData.description}
+            ></TextArea>
+            <FormButtonsContainer>
+              <EditButton
+                type="submit"
+                className="material-symbols-outlined link"
+              >
+                done
+              </EditButton>
+              <EditButton
+                onClick={changeEditing}
+                className="material-symbols-outlined link"
+                settings
+              >
+                close
+              </EditButton>
+              {errors.description?.type === 'maxLength' && (
+                <ErrorSpan role="alert">
+                  Description only take 200 characters.
+                </ErrorSpan>
+              )}
+              {errors.server?.type === 'serverError' && (
+                <ErrorSpan role="alert">{errors.server?.message}</ErrorSpan>
+              )}
+            </FormButtonsContainer>
+          </form>
+        ) : (
+          <>
+            <UserDescriptionSpan>{userData.description}</UserDescriptionSpan>
+            {userContext.user.username === user && (
+              <EditButton
+                onClick={changeEditing}
+                className="material-symbols-outlined link"
+                settings
+              >
+                settings
+              </EditButton>
+            )}
+          </>
+        )}
+      </Container>
     </UserDescriptionContainer>
   );
 }
